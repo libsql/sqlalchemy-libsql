ded206cfe5a9c54b5c22b5d0e5cad1a1e021aaea
Douglas Montes <douglas.montes@profusion.mobi>
2023-10-03 13:31:23 -0300

    feat(async_dialect): add SQLiteDialect_libsql_async
    - provides compatibility with create_async_engine and async_session

--- sqlalchemy_libsql/__init__.py
+++ sqlalchemy_libsql/__init__.py
@@ -1,8 +1,17 @@
 from sqlalchemy.dialects import registry as _registry
+
+from sqlalchemy_libsql.libsql import dialect
 from sqlalchemy_libsql.libsql import SQLiteDialect_libsql
+from sqlalchemy_libsql.libsql_async import SQLiteDialect_libsql_async
 
 __version__ = "0.1.0-pre"
 
 _registry.register(
     "sqlite.libsql", "sqlalchemy_libsql", "SQLiteDialect_libsql"
 )
+
+_registry.register(
+    "sqlite.libsql_async", "sqlalchemy_libsql", "SQLiteDialect_libsql_async"
+)
+
+__all__ = ("SQLiteDialect_libsql", "SQLiteDialect_libsql_async", "dialect")
